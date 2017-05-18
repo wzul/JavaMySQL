b46b63b64e113816c643eb0d7c47aa5beec57605
Zhamri Che Ani <zhamri@gmail.com>
2017-05-18 16:02:57 +0800

    Delete record

---
+++ src/MysqlDeleteRecord.java
@@ -0,0 +1,26 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class MysqlDeleteRecord {
+
+    static final String DB_URL = "jdbc:mysql://sql12.freesqldatabase.com:3306/sql12174999";
+    static final String USER = "sql12174999";
+    static final String PASSWORD = "EYX8tM6xxx";
+
+    public static void main(String[] args) throws SQLException {
+
+        Connection connection = DriverManager.getConnection(DB_URL, USER, PASSWORD);
+        Statement stmt = connection.createStatement();
+
+        String sql = "DELETE FROM student "
+                + "WHERE matric = 55555";
+        stmt.executeUpdate(sql);
+        System.out.println("Record has been deleted");
+
+        stmt.close();
+        connection.close();
+    }
+}
